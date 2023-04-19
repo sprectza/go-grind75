b6a1ed9609ecac78855ad62a8ade414852d860b2
sprectza <43305013+sprectza@users.noreply.github.com>
2023-04-19 15:01:49 +0530

    Create 12_linked_list_cycle.go

---
+++ 12_linked_list_cycle.go
@@ -0,0 +1,27 @@
+// Problem desc: https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    if head == nil {
+		return false
+	}
+
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false 
+}
